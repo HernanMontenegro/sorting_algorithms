3964516f9d03af294bc89b9899671b02a27dfaa7
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-10 23:38:37 +0000

    starting 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers using bubble sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ * --------------------------------------
+*/
+void bubble_sort(int *array, size_t size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+
+    }
+}
