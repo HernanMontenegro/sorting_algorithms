d36d71b08c9a0ed10a3319918122c9885f2bbbdf
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-11 18:59:18 +0000

    YES!

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,13 +3,13 @@
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers
- * 						 in ascending order using the Insertion sort algorithm
- * @list: the list to sort 
+ *					 in ascending order using the Insertion sort algorithm
+ * @list: the list to sort
  * --------------------------------------
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *cu, *cu_p, *aux;
+	listint_t *cu, *cu_p;
 
 	if (!list)
 		return;
@@ -17,36 +17,30 @@ void insertion_sort_list(listint_t **list)
 	while (cu)
 	{
 		cu_p = cu->prev;
-		if (cu->n >= cu_p->n)
+		while (cu_p && cu->n < cu_p->n)
 		{
-			cu = cu->next;
+			if (!cu_p->prev)
+			{
+				cu->next->prev = cu->prev;
+				cu->prev->next = cu->next;
+				cu_p->prev = cu;
+				cu->prev = NULL;
+				cu->next = cu_p;
+				(*list) = cu;
+			}
+			else
+			{
+				cu->prev->next = cu->next;
+				if (cu->next)
+					cu->next->prev = cu->prev;
+				cu->next = cu_p;
+				cu_p->prev->next = cu;
+				cu->prev = cu_p->prev;
+				cu_p->prev = cu;
+			}
+			print_list(*list);
 			cu_p = cu->prev;
-			continue;
 		}
-		while (cu_p->prev && cu->n < cu_p->prev->n)
-			cu_p = cu_p->prev;
-		if (!cu_p->prev)
-		{
-			cu->next->prev = cu->prev;
-			cu->prev->next = cu->next;
-			aux = cu->next;
-			cu_p->prev = cu;
-			cu->prev = NULL;
-			cu->next = cu_p;
-			(*list) = cu;
-			cu = aux;
-		}
-		else
-		{
-			cu_p->next = cu->next;
-			if (cu->next)
-				cu->next->prev = cu_p;
-			cu->next = cu_p;
-			cu->prev = cu_p->prev;
-			cu_p->prev->next = cu;
-			cu_p->prev = cu;
-			cu = cu_p->next;
-		}
-		print_list(*list);
+		cu = cu->next;
 	}
 }
