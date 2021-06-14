dce3e871f3b861e884c478eadae505f09b13f24e
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-14 20:48:49 +0000

    a

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 void quick_sort_3070(int *arr, size_t st, size_t ed);
+size_t division(int *arr, size_t st, size_t up);
 
 /**
  * quick_sort - quick sort sorting order
@@ -14,39 +15,63 @@ void quick_sort(int *array, size_t size)
 	quick_sort_3070(array, 0, size - 1);
 }
 
-void quick_sort_3070(int *arr, size_t st, size_t ed)
+/**
+ * quick_sort_3070 - the real quick sort function
+ * @arr: the array
+ * @lb: lower bound (the min index)
+ * @ub: upper bound (the max index)
+ * --------------------------------------------
+*/
+void quick_sort_3070(int *arr, size_t lb, size_t ub)
 {
-	int pivot, i, j, aux;
+	size_t loc;
 
-	if (!arr)
-		return;
-	pivot = array[ed];
-
-	for (; st < size; st++)
+	if (lb < ub)
 	{
-		if (pivot >= array[st])
-			continue;
-		
-		while (ed >= 0)
-		{
-			if (st > ed)
-			{
-				aux = array[ed];
-				array[ed] = pivot;
-				array[size - 1] = aux;
-				break;
-			}
-			if (pivot < array[ed])
-			{
-				ed--;
-				continue;
-			}
-			aux = array[st];
-			array[st] = array[ed];
-			array[ed] = array[st];
-			break;
-		}
-		/* for avoid increment */
-		st--;
+		loc = division(arr, lb, ub);
+		quick_sort_3070(arr, lb, loc - 1);
+		quick_sort_3070(arr, loc + 1, ub);
 	}
 }
+
+/**
+ * division - manages the array partitions
+ * @arr: the array
+ * @st: starting path index
+ * @up: upper bound (last index)
+ * ----------------------------------
+ * Return: location of the pivot index
+*/
+size_t division(int *arr, size_t st, size_t up)
+{
+	int pivot, aux;
+	size_t ed = up;
+
+	if (!arr)
+		return (-1);
+	pivot = arr[ed];
+	while (st < ed)
+	{
+
+		while (arr[st] <= pivot)
+			st++;
+		while (arr[ed] > pivot)
+			ed--;
+
+		if (st < ed)
+		{
+			aux = arr[st];
+			arr[st] = arr[ed];
+			arr[ed] = aux;
+			print_array(arr, up);
+		}
+	}
+
+	aux = arr[ed];
+	arr[ed] = pivot;
+	arr[up] = aux;
+	print_array(arr, up);
+
+	return (ed);
+}
+	
